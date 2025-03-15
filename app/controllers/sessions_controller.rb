b8f116860528a537c463fcdbd5c3e7919b6b7f16
rjmmoore Moore <rob.moore@kellogg.northwestern.edu>
2025-03-15 22:43:27 +0000

    User authentication complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,42 @@
+# class SessionsController < ApplicationController
+#   def new
+#   end
+
+#   def create
+#     @user = User.find_by({ "email" => params["email"] })
+#     if @user
+#       if BCrypt::Password.new(@user["password"]) == params["password"]
+#         session["user_id"] = @user["id"] 
+#         flash["notice"] = "You've logged in."
+#         redirect_to "/places"
+#       else
+#         flash["notice"] = "Unsuccessful login."
+#         redirect_to "/sessions/new"
+#       end
+#     else
+#       flash["notice"] = "Unsuccessful login."
+#       redirect_to "/sessions/new"
+#   end
+
+#   def destroy
+#     session["user_id"] = nil
+#     flash["notice"] = "You've logged out."
+#     redirect_to "/login" 
+#   end
+# end
+# end
+
+
 class SessionsController < ApplicationController
   def new
   end
 
   def create
     @user = User.find_by({ "email" => params["email"] })
+
     if @user
-      if BCrypt::Password.new(@user["password"]) == params["password"] 
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
         flash["notice"] = "You've logged in."
         redirect_to "/places"
       else
@@ -15,9 +46,13 @@ class SessionsController < ApplicationController
     else
       flash["notice"] = "Unsuccessful login."
       redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session["user_id"] = nil  # Clear the session (logs user out)
+    flash["notice"] = "You've logged out."
+    redirect_to "/login"  # Redirect back to login page
   end
 end
-end
+
