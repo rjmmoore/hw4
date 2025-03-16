e95dda547dd23e1fd215486bf70b428ab24fe287
rjmmoore Moore <rob.moore@kellogg.northwestern.edu>
2025-03-16 02:15:31 +0000

    Working on user authorization

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,16 +1,62 @@
 class EntriesController < ApplicationController
-
   def new
+    if @current_user.nil?
+      flash["notice"] = "You must be logged in to add an entry."
+      redirect_to "/login"
+    end
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    if @current_user.nil?
+      flash["notice"] = "You must be logged in to add an entry."
+      redirect_to "/login"
+    else
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = @current_user["id"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    end
   end
 
+  def edit
+    @entry = Entry.find_by({ "id" => params["id"] })
+
+    if @current_user.nil? || @entry["user_id"] != @current_user["id"]
+      flash["notice"] = "You are not authorized to edit this entry."
+      redirect_to "/places"
+    end
+  end
+
+  def update
+    @entry = Entry.find_by({ "id" => params["id"] })
+
+    if @current_user.nil? || @entry["user_id"] != @current_user["id"]
+      flash["notice"] = "You are not authorized to update this entry."
+      redirect_to "/places"
+    else
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    end
+  end
+
+  def destroy
+    @entry = Entry.find_by({ "id" => params["id"] })
+
+    if @current_user.nil? || @entry["user_id"] != @current_user["id"]
+      flash["notice"] = "You are not authorized to delete this entry."
+      redirect_to "/places"
+    else
+      @entry.destroy
+      flash["notice"] = "Entry deleted."
+      redirect_to "/places/#{@entry["place_id"]}"
+    end
+  end
 end
+
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,12 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+
+    if @current_user
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] }) # Only show current user's entries
+    else
+      @entries = [] # Show no entries if logged out
+    end
   end
 
   def new
@@ -20,3 +25,4 @@ class PlacesController < ApplicationController
   end
 
 end
+
